85b860ace913fb1ff496fd568886005c9ff91b31
JCoupalK <108779415+JCoupalK@users.noreply.github.com>
2023-06-02 15:19:11 -0400

    Rewritten similar to KPing.

--- KStatus.sh
+++ KStatus.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-YEL=$'\e[1;33m'
-RED=$'\033[0;31m'
-NC=$'\033[0m' # No Color
-PRPL=$'\033[1;35m'
-GRN=$'\e[1;32m'
-BLUE=$'\e[3;49;34m'
+YEL=$'\e[1;33m'     # Yellow
+RED=$'\033[0;31m'   # Red
+NC=$'\033[0m'       # No Color
+PRPL=$'\033[1;35m'  # Purple
+GRN=$'\e[1;32m'     # Green
+BLUE=$'\e[3;49;34m' # Blue
 
+#script logo with copyrights
 printf "${BLUE}\n"
 echo "██╗  ██╗███████╗████████╗ █████╗ ████████╗██╗   ██╗███████╗"
 echo "██║ ██╔╝██╔════╝╚══██╔══╝██╔══██╗╚══██╔══╝██║   ██║██╔════╝"
@@ -16,66 +17,145 @@ echo "╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚�
 printf "\nPowered by KeepSec Technologies Inc.™\n"
 printf "${NC}\n"
 
-sudo -v &> /dev/null || printf "${RED}\nThis script must be run with sudo privileges${NC}\n";
-printf "${GRN}\nThis script must be run with 'autoexpect' (see https://github.com/KeepSec-Technologies/KStatus)${NC}\n"
+#check if root or not
+if [ $(id -u) -ne 0 ]; then
+  printf "${RED}\nThis script can only be executed as root\n\n${NC}"
+  sleep 0.5
+  exit
+fi
 sleep 1
-echo""
-echo ""
+printf "\n"
 
-read -p "How often in ${YEL}minutes${NC} do you want the cron job to run (0-60) : " cron
-echo""
+#make directories and variables for it
+mkdir /etc/KStatus &>/dev/null
+mkdir /etc/KStatus/logs &>/dev/null
+mkdir /etc/KStatus/configs &>/dev/null
+dirConf="/etc/KStatus/configs"
+dirLogs="/etc/KStatus/logs"
+
+#variables for number of questions
+num=1
+
+#variables of quotes to output secondary scripts well
+sqt="'"
+dqt='"'
+
+#where configs are localted
+config=($dirConf/KStatus-*)
+
+#function to ask if you want to delete previous configs
+function delConf {
+  read -n 1 -p "${YEL}Delete${NC} previous KSatus configurations? (Y/N) " ynDEL
+  printf "\n\nThose configurations will be ${RED}deleted${NC}:\n\n"
+  ls -A1 $dirConf | awk -F 'KStatus-|-job' {'print $2'}
+  case $ynDEL in
+  [yY])
+    printf "\n"
+    read -n 1 -p "${RED}Are you ${RED}sure?${NC} (Y/N) " ynDEL2
+    printf "\n"
+    ;;
+  [nN])
+    printf "\n"
+    ;;
+  *)
+    printf "${RED}\n\nAnswer 'Y' or 'N', try again\n\n${NC}"
+    sleep 0.5
+    delConf
+    ;;
+  esac
+  case $ynDEL2 in
+  [yY])
+    printf "\n"
+    ;;
+  [nN])
+    printf "\n"
+    delConf
+    ;;
+  *)
+    printf "${RED}\nAnswer 'Y' or 'N', try again\n\n${NC}"
+    sleep 0.5
+    delConf
+    ;;
+  esac
+}
+#function to ask frequence of checking the hosts (min 1 minute, max 60 minutes)
+function askFreq {
+  read -n 2 -p "How often in ${YEL}minutes${NC} do you want the cron job to run (1-60) : " cron
+  case $cron in
+  *)
+    if [[ $cron -gt 0 && $cron -lt 61 ]]; then
+      printf "\n\n"
+    else
+      printf "${RED}\nOnly numbers between 1 and 60, try again\n\n${NC}"
+      sleep 0.5
+      askFreq
+    fi
+    ;;
+  esac
+}
+#function to ask for another monitor or not
+function askMonit {
+  read -n 1 -p "Do you want another service (${YEL}${num}${NC})? (Y/N) " yn
+  case $yn in
+  [yY])
+    printf "\n\n"
+    ;;
+  [nN])
+    printf "\n\n"
+    return 1
+    ;;
+  *)
+    printf "${RED}\nAnswer 'Y' or 'N', try again\n\n${NC}"
+    sleep 0.5
+    askMonit
+    ;;
+  esac
+}
+
+#check to see if configuration already present and run delConf function if it is
+if [ -f "${config[0]}" ]; then
+  delConf
+  if [[ $ynDEL2 == Y || $ynDEL2 == y ]]; then
+    rm -f /etc/cron.d/KStatus-*
+    rm -f $dirConf/KStatus-*
+  fi
+fi
+#start of series of questions
+askFreq
+#email address to receive the notifs
 read -p "What is the ${YEL}email${NC} address that you want to receive your notifications : " to
 echo""
+#internet domain pointing to your server to send emails
 read -p "What is the ${YEL}domain${NC} that will be used to send emails : " domain
 echo""
-printf "${GRN}Assure yourself that the domain is pointing to the IP of your server${NC}\n"
-echo ""
+printf "${GRN}Assure yourself that the domain is pointing to the IP of your server with preferably a SPF/DMARC${NC}\n"
+echo""
 sleep 1
+#loop for infinite questions to add as many services as you want
+while :; do
+  read -p "What is the (${YEL}${num}${NC}) service you want notifications for : " statusloop
+  echo""
+  echo -e "#!/bin/bash
+        subject=${dqt}SERVICE DOWN: $statusloop${dqt}
+        status=${dqt}\$(sudo systemctl show $statusloop --no-page)${dqt}
+        status_text=\$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
+        if [[ ${dqt}\${status_text}${dqt} == ${dqt}100% packet loss${dqt} ]]; then
+        printf ${dqt}The service ${sqt}$statusloop${sqt} is currently down!\n\n Please check it out as soon as possible.${dqt} | mail -r ${dqt}notification${dqt} -s ${dqt}\$subject${dqt} ${dqt}$to${dqt}
+        fi" >$dirConf/KStatus-$statusloop-job.sh
+  croncmd="root /usr/bin/bash $dirConf/KStatus-$statusloop-job.sh >> $dirLogs/KStatus-$statusloop.log"
+  cronjob="*/$cron * * * * $croncmd"
 
-read -p "What is the (${YEL}1${NC}) service you want to get notifications for : " service1
-echo""
-read -p "Do you want a second service? (Y/N) " yn1
+  printf "$cronjob\n" >"/etc/cron.d/KStatus-$statusloop-job"
+  num=$((num + 1))
+  askMonit || break
+done
 
-if [[ $yn1 == Y || $yn1 == y ]]; then 
-echo""
-read -p "What is the (${YEL}2${NC}) service you want to get notifications for : " service2
-echo""
-read -p "Do you want a third service? (Y/N) " yn2
-echo""
-    if [[ $yn2 == Y || $yn2 == y ]]; then 
-    read -p "What is the (${YEL}3${NC}) service you want to get notifications for : " service3
-    echo""
-    read -p "Do you want a fourth service? (Y/N) " yn3
-    echo""
-      if [[ $yn3 == Y || $yn3 == y ]]; then 
-        echo""
-        read -p "What is the (${YEL}4${NC}) service you want to get notifications for : " service4
-        echo""
-        read -p "Do you want a fifth service? (Y/N) " yn4
-        e
-        echo""
-        if [[ $yn4 == Y || $yn4 == y ]]; then 
-        echo""
-        read -p "What is the (${YEL}5${NC}) service you want to get notifications for : " service5
-        echo""
-        fi
-      fi
-    fi
-fi 
-
-if [[ $yn1 == N || $yn1 == n ]]; then 
-
-echo""
-
-fi
-
+#function for the installing wheel
 function installing {
   tput civis
   spinner="⣾⣽⣻⢿⡿⣟⣯⣷"
-  while :
-  do
-    for i in `seq 0 7`
-    do
+  while :; do
+    for i in $(seq 0 7); do
       printf "${PRPL}${spinner:$i:1}"
       printf "\010${NC}"
       sleep 0.2
@@ -83,127 +163,58 @@ function installing {
   done
 }
 
+#executes function above
 installing &
 SPIN_PID=$!
 disown
-printf "${PRPL}\nInstalling Postfix and Mailx ➜ ${NC}"
+printf "${PRPL}\nInstalling utilities ➜ ${NC}"
 
-if [ -n "`command -v apt-get`" ]; then
-
-sudo apt-get install -y debconf &> /dev/null
-echo "postfix postfix/mailname string $domain" | debconf-set-selections
-echo "postfix postfix/protocols select  all" | debconf-set-selections
-echo "postfix postfix/mynetworks string  127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128" | debconf-set-selections
-echo "postfix postfix/mailbox_limit string  0" | debconf-set-selections
-echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
-echo "postfix postfix/compat_conversion_warning   boolean true" | debconf-set-selections
-echo "postfix postfix/protocols select all" | debconf-set-selections
-echo "postfix postfix/procmail boolean false" | debconf-set-selections
-echo "postfix postfix/relayhost string" | debconf-set-selections
-echo "postfix postfix/chattr boolean false" | debconf-set-selections
-echo "postfix postfix/destinations string  $domain" | debconf-set-selections
-
+#checks package manager and then install all the necessary utilities with your right package manager + puts the right configuration in the config file for postfix for you
+if [ -n "$(command -v apt-get)" ]; then
+  sudo apt-get -y purge postfix &>/dev/null
+  sudo echo "postfix postfix/mailname string $domain" | debconf-set-selections
+  sudo echo "postfix postfix/protocols select  all" | debconf-set-selections
+  sudo echo "postfix postfix/mynetworks string  127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128" | debconf-set-selections
+  sudo echo "postfix postfix/mailbox_limit string  0" | debconf-set-selections
+  sudo echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
+  sudo echo "postfix postfix/compat_conversion_warning   boolean true" | debconf-set-selections
+  sudo echo "postfix postfix/protocols select all" | debconf-set-selections
+  sudo echo "postfix postfix/procmail boolean false" | debconf-set-selections
+  sudo echo "postfix postfix/relayhost string" | debconf-set-selections
+  sudo echo "postfix postfix/chattr boolean false" | debconf-set-selections
+  sudo echo "postfix postfix/destinations string $domain" | debconf-set-selections
 fi
 
-if [ -n "`command -v apt-get`" ];
-
-then sudo apt-get -y install postfix > /dev/null  && sudo apt-get -y install bsd-mailx > /dev/null; 
-
-elif [ -n "`command -v yum`" ]; 
-
-then sudo yum install -y postfix > /dev/null  && sudo yum install -y mailx > /dev/null; 
-
-elif [ -n "`command -v pacman`" ];
-
-then sudo pacman -y install postfix > /dev/null  && sudo pacman install -y mailx > /dev/null; 
-
+if [ -n "$(command -v apt-get)" ]; then
+  sudo apt-get -y install postfix >/dev/null && sudo apt-get -y install bsd-mailx >/dev/null
+elif [ -n "$(command -v yum)" ]; then
+  sudo yum remove -y postfix &>/dev/null && sudo yum install -y postfix >/dev/null && sudo yum install -y mailx >/dev/null
+elif [ -n "$(command -v pacman)" ]; then
+  sudo pacman -S postfix >/dev/null && sudo pacman -S mailx >/dev/null
 fi
 
-sudo adduser --disabled-password --gecos "" notification &> /dev/null
+sudo adduser --disabled-password --gecos "" notification &>/dev/null
 
-if [ -n "`command -v yum`" ]; then
-
-sudo sed -i -e "s/inet_interfaces = localhost/inet_interfaces = all/g" /etc/postfix/main.cf &> /dev/null
-sudo sed -i -e "s/#mydomain =.*/mydomain = $domain/g" /etc/postfix/main.cf &> /dev/null 
-sudo sed -i -e "s/#myorigin = $mydomain/myorigin = $mydomain/g" /etc/postfix/main.cf &> /dev/null
-sudo sed -i -e "s/#mynetworks =.*/mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128/g" /etc/postfix/main.cf &> /dev/null
-sudo sed -i -e "s/mydestination =.*/mydestination = mail."'$mydomain'", "'$mydomain'"/g" /etc/postfix/main.cf &> /dev/null
-sudo sed -i -e "117d" /etc/postfix/main.cf &> /dev/null
-
+if [ -n "$(command -v yum)" ]; then
+  sudo sed -i -e "s/inet_interfaces = localhost/inet_interfaces = all/g" /etc/postfix/main.cf &>/dev/null
+  sudo sed -i -e "s/#mydomain =.*/mydomain = $domain/g" /etc/postfix/main.cf &>/dev/null
+  sudo sed -i -e "s/#myorigin = $mydomain/myorigin = $mydomain/g" /etc/postfix/main.cf &>/dev/null
+  sudo sed -i -e "s/#mynetworks =.*/mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128/g" /etc/postfix/main.cf &>/dev/null
+  sudo sed -i -e "s/mydestination =.*/mydestination = mail."'$mydomain'", "'$mydomain'"/g" /etc/postfix/main.cf &>/dev/null
+  sudo sed -i -e "117d" /etc/postfix/main.cf &>/dev/null
 fi
 
-sudo systemctl enable postfix &> /dev/null
-sudo systemctl start postfix > /dev/null
-sudo systemctl reload postfix > /dev/null
+#enable postfix mail services
+sudo systemctl enable postfix &>/dev/null
+sudo systemctl start postfix &>/dev/null
+sudo systemctl reload postfix &>/dev/null
 
-#1
-subject1="SERVICE DOWN: $service1"
-status1="$(sudo systemctl show $service1 --no-page)"
-status_text1=$(echo "${status1}" | grep 'ActiveState=' | cut -f2 -d=)
-
-
-if [ "${status_text1}" == "inactive" ]; then
-
-printf "The service '$service1' is currently down!\n\n Please check it out." | mail -r "notification" -s "$subject1" "$to"
-
-fi
-
-#2
-subject2="SERVICE DOWN: $service2"
-status2="$(sudo systemctl show $service2 --no-page)"
-status_text2=$(echo "${status2}" | grep 'ActiveState=' | cut -f2 -d=)
-
-if [ "${status_text2}" == "inactive" ]; then
-
-printf "The service '$service2' is currently down!\n\n Please check it out." | mail -r "notification" -s "$subject2" "$to"
-
-fi
-
-#3
-subject3="SERVICE DOWN: $service3"
-status3="$(sudo systemctl show $service3 --no-page)"
-status_text3=$(echo "${status3}" | grep 'ActiveState=' | cut -f2 -d=)
-
-if [ "${status_text3}" == "inactive" ]; then
-
-printf "The service '$service3' is currently down!\n\n Please check it out." | mail -r "notification" -s "$subject3" "$to"
-
-fi
-
-#4
-subject4="SERVICE DOWN: $service4"
-status4="$(sudo systemctl show $service4 --no-page)"
-status_text4=$(echo "${status4}" | grep 'ActiveState=' | cut -f2 -d=)
-
-if [ "${status_text4}" == "inactive" ]; then
-
-printf "The service '$service4' is currently down!\n\n Please check it out." | mail -r "notification" -s "$subject4" "$to"
-
-fi
-
-#5
-subject5="SERVICE DOWN: $service5"
-status5="$(sudo systemctl show $service5 --no-page)"
-status_text5=$(echo "${status5}" | grep 'ActiveState=' | cut -f2 -d=)
-
-if [ "${status_text5}" == "inactive" ]; then
-
-printf "The service '$service5' is currently down!\n\n Please check it out." | mail -r "notification" -s "$subject5" "$to"
-
-fi
-
-sudo chmod +x script.exp &> /dev/null
-$Path1=$(echo $PWD)
-
-croncmd="/usr/bin/expect $Path1/script.exp &> /dev/null"
-cronjob="*/$cron * * * * $croncmd"
-
-( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
-
-kill -9 $SPIN_PID &> /dev/null
+#kills spinning wheel
+kill -9 $SPIN_PID &>/dev/null
 tput cnorm
 echo ""
+#bye bye message :)
 printf "${GRN}\n\nWe're done!${NC}"
 echo ""
 
-exit 0
+exit 1
